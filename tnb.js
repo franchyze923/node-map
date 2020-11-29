7ce2ba3baf57acad2192d861dda74889f1f14d36
Fran Polignano <fpolignano@villanova.edu>
2020-11-29 08:13:28 -0500

    updated with new PV IP
    updated with new PV IP

--- tnb.js
+++ tnb.js
@@ -1,27 +1,30 @@
 var map = L.map('mapid').setView([37, 0], 2.5);
 L.esri.basemapLayer('DarkGray').addTo(map);
 
-function getBanks() {
+function getNodeInfo(request) {
 
-    fetch('http://54.183.17.224/banks?limit=30&offset=0')
+    fetch(request)
         .then(response => response.json())
-        .then(function (data) {
-            for (var x = 0; x < data.count; x++) {
-                getLocation(data.results[x].ip_address)
-            }
+        .then(function (node_data) {
+            getLocation(node_data)
         })
+
 }
 
-function getLocation(ip_address) {
+function getLocation(node_data) {
 
-    fetch('http://ip-api.com/json/' + ip_address)
+    for (var x = 0; x < node_data.count; x++) {
+        
+        fetch('http://ip-api.com/json/' + node_data.results[x].ip_address)
         .then(response => response.json())
-        .then(function (data) {
-            data.lat = data.lat + Math.random() * (0.2 - 0.1) + 0.1
-            var combo = "IP Address: " + data.query + "<br>" + "Country: " + data.country + "<br>" + "City: " + data.city + "<br>" + "Node Type: Bank"
-            var marker = L.marker([data.lat, data.lon]).addTo(map);
+        .then(function (ip_data) {
+            ip_data.lat = ip_data.lat + Math.random() * (0.2 - 0.1) + 0.1
+            var combo = "IP Address: " + ip_data.query + "<br>" + "Country: " + ip_data.country + "<br>" + "City: " + ip_data.city + "<br>" + "Node Type:" + " Validator" + "<br>" + "Provider: " + ip_data.org
+            var marker = L.marker([ip_data.lat, ip_data.lon]).addTo(map);
             marker.bindPopup(combo);
+            console.log(ip_data)
         })
+    }
 }
 
-getBanks()
+getNodeInfo("http://157.230.75.212/validators?limit=30&offset=0")
